184e574c86b9f9c7677ebac9b972d3f7dbc0700e
alvarez98 <aeap19980929@gmail.com>
2020-03-25 10:44:12 -0600

    Agregando ruta diagnosis

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'HELLO'
-    
+@app.route('/diagnosis', methods = ['POST'])
+def diagnosis():
+    if('values' in request.json and type(request.json['values']) == list):
+        print(request.json['values'])
+    return 'Hola'
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'error', 404
+
 if __name__ == "__main__":
     app.run(port = 3000, debug = True)
